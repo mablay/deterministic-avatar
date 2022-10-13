70d1e6bedb856143e88ab015db79505a8da56be4
Marc Ablay <marc.ablay@gmail.com>
2022-10-14 00:19:08 +0200

    fixing production publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // @ts-ignore
+  publicPath: process.env.NODE_ENV === 'production' ? '/deterministic-avatar/' : '/'
 })
